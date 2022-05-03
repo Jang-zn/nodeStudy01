c2535e0ccdea4ab267560c454f62e117022dd4db
Jang <zn2309@naver.com>
2022-05-03 11:38:52 +0900

    header 설정 및 html 파일 read -> response

--- 4/server1.js
+++ 4/server1.js
@@ -1,11 +1,19 @@
 const http = require('http');
+const fs = require('fs').promises;
 
-http.createServer((req, res)=>{
-    res.write('<h1>Hello Node</h1>');
-    res.write('<p>Hello Http</p>');
-
+http.createServer(async (req, res)=>{
+    try{
+    //기본 세팅.. 헤더 설정 ( html / utf-8 설정)
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        const data = await fs.readFile('./server1.html');
+        res.end(data);
+    }catch(error){
+        console.error(error);
+        res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+        res.end(error.message);
+    }
 })
-//8888포트에 연결하고 대기중인 상태로 만들어줌
+//8080포트에 연결하고 대기중인 상태로 만들어줌
 .listen(8080, ()=>{
     console.log('8080 port waiting');
 });
