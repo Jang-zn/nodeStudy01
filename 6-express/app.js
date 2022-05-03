6900dc7e78f8ddeffe2374723d4442b3b0595d9f
Jang <zn2309@naver.com>
2022-05-03 15:05:03 +0900

    middleware - app.use((req, res, next)=>{ next();})

--- 6-express/app.js
+++ 6-express/app.js
@@ -2,11 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port',process.env.PORT||3000);
+app.set('port',8080);
+app.use((req, res, next)=>{
+    console.log('모든 요청에 실행');
+    next();
+});
+
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 });
+
+app.get('/about',(req,res)=>{
+    res.send("hello about");
+});
+
+app.get('/:name',(req,res)=>{
+    res.send(`hello ${req.params.name}`);
+});
+
+
 
 app.listen(app.get('port'), ()=>{
     console.log('express start');
