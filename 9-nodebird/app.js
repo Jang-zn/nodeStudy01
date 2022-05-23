55e11d56479f3f7f0659f5ef29f1e67cf1ce490e
Jang <zn2309@naver.com>
2022-05-23 17:34:39 +0900

    app.js 배포용 설정 - helmet, hpp

--- 9-nodebird/app.js
+++ 9-nodebird/app.js
@@ -15,12 +15,10 @@ const {sequelize} = require('./models');
 
 //passport 인증관련 폴더 설정
 const passportConfig = require('./passport');
-
-
-
 const app = express();
 app.set('port', process.env.PORT || 3001);
 app.set('view engine', 'html');
+
 nunjucks.configure('views', {
     express: app,
     watch: true,
@@ -37,22 +35,41 @@ sequelize.sync({ force: false })
   //↓ 이거 해줘야 passport 사용 가능
   passportConfig();
 
+if(process.env.NODE_ENV==='production'){
+  app.use(morgan('combined'));
+  //세션에서 proxy true인 경우 추가
+  app.enable('trust proxy');
+  //
+  app.use(helmet());
+  //
+  app.use(hpp());
+}else{
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-    resave:false,
-    saveUninitialized:false,
-    secret:process.env.COOKIE_SECRET,
-    cookie : {
-        httpOnly:true,
-        secure:false,
-    }
-}));
+
+//배포용으로 세션설정
+const sessionOption = {
+  resave:false,
+  saveUninitialized:false,
+  secret:process.env.COOKIE_SECRET,
+  cookie : {
+      httpOnly:true,
+      secure:false,
+  }
+}
+if(process.env.NODE_ENV === 'production'){
+  //노드 앞에 별개 서버가 있는경우에만 proxy=true 실무에선 nginx같은거 있어서 true 해놓으면 좋음
+  sessionOption.proxy = true;
+  //http 적용시에는 secure = true 추가
+  //sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
 
 
 //Router 연결전에 passport 미들웨어 연결해줘야 한다.
